a4839786e8bd622e452ac66c8d3b85b3193a0668
trentduncan <trent.dunc@gmail.com>
2018-03-01 11:13:04 -0800

    added sample store obj for default state with two items

---
+++ scripts/store.js
@@ -0,0 +1,32 @@
+'use strict';
+
+/* global cuid */
+
+//eslint-disable-next-line no-unused-vars
+const store = (function(){
+  const bookmarks = [
+    {title: 'Bookmark1',
+      description: 'Filler descsdfgafgfdsgfdsgdfsgsd',
+      url: 'https//:www.testurl.com',
+      rating: 5,
+      expanded: false,
+      id: cuid()
+    },
+    {title: 'Bookmark2',
+      description: 'filler2 descsdfgafgfdsgfdsgdfsgsd',
+      url: 'https//:www.testurl2.com',
+      rating: 3,
+      expanded: false,
+      id: cuid()
+    }
+  ];
+  let ratingFilter = null;
+  let addBookmark = true;
+
+
+  return {
+    bookmarks,
+    ratingFilter,
+    addBookmark
+  };
+}());

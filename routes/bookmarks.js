e388648b458423ae973eb03e1b4a360157106d2a
trentduncan <trent.dunc@gmail.com>
2018-03-28 11:48:28 -0700

    added post and delete endpoints

--- routes/bookmarks.js
+++ routes/bookmarks.js
@@ -15,4 +15,44 @@ router.get('/bookmarks', (req, res, next) => {
     .catch(next);
 });
 
+router.post('/bookmarks', ( req, res, next) => {
+  const {title, desc, url, rating} = req.body;
+
+  if (!title) {
+    const err = new Error('Missing `title` in request body');
+    err.status = 400;
+    return next(err);
+  }
+  if (!url) {
+    const err = new Error('Missing `url` in request body');
+    err.status = 400;
+    return next(err);
+  }
+
+  const newItem = {
+    title,
+    desc,
+    url,
+    rating
+  };
+
+  Bookmark.create(newItem)
+    .then(result => {
+      res.location(`${req.originalUrl}/${result.id}`).status(201).json(result);
+    })
+    .catch(err => {
+      next(err);
+    });
+});
+
+router.delete('/bookmarks/:id', (req, res, next) => {
+  const {id} = req.params;
+
+  Bookmark.findByIdAndRemove(id)
+    .then(() => {
+      res.status(204).end();
+    })
+    .catch(next);
+});
+
 module.exports = router;

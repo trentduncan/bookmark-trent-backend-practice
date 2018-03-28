c7afce323a31c1cc691ee61358abb394c73a5d3f
trentduncan <trent.dunc@gmail.com>
2018-03-28 11:28:28 -0700

    implemented get all route

--- routes/bookmarks.js
+++ routes/bookmarks.js
@@ -3,4 +3,16 @@
 const express = require('express');
 const router = express.Router();
 
-const mongoose = require('mongoose');
+// const mongoose = require('mongoose');
+
+const Bookmark = require('../models/bookmark');
+
+router.get('/bookmarks', (req, res, next) => {
+  Bookmark.find()
+    .then(results => {
+      res.json(results);
+    })
+    .catch(next);
+});
+
+module.exports = router;
--- server.js
+++ server.js
@@ -2,8 +2,9 @@
 
 const express = require('express');
 const morgan = require('morgan');
+const mongoose = require('mongoose');
 
-const { PORT } = require('./config');
+const { PORT, MONGODB_URI } = require('./config');
 
 const bookmarksRouter = require('./routes/bookmarks');
 const app = express();
@@ -33,8 +34,21 @@ app.use(function (err, req, res, next) {
 });
 
 
-app.listen(PORT, function () {
-  console.info(`Server listening on ${this.address().port}`);
-}).on('error', err => {
-  console.error(err);
-});
+if (require.main === module) {
+  mongoose.connect(MONGODB_URI)
+    .then(instance => {
+      const conn = instance.connections[0];
+      console.info(`Connected to: mongodb://${conn.host}:${conn.port}/${conn.name}`);
+    })
+    .catch(err => {
+      console.error(`ERROR: ${err.message}`);
+      console.error('\n === Did you remember to start `mongod`? === \n');
+      console.error(err);
+    });
+  
+  app.listen(PORT, function () {
+    console.info(`Server listening on ${this.address().port}`);
+  }).on('error', err => {
+    console.error(err);
+  });
+}
